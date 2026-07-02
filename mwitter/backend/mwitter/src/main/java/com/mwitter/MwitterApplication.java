6475894588d40d5411f77f00b2dcfb2bbe56bce4
mervevarlibas <mervevarlibas09@gmail.com>
2026-07-02 14:15:52 +0300

    spring boot dosyalarını ekledim

---
+++ mwitter/backend/mwitter/src/main/java/com/mwitter/MwitterApplication.java
@@ -0,0 +1,13 @@
+package com.mwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwitterApplication.class, args);
+	}
+
+}
---
+++ mwitter/backend/mwitter/src/test/java/com/mwitter/MwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
